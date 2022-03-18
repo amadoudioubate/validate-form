a0dba6f1a3a0553d8ea14f381359669f28dad0b5
amadoudioubate <dioubateamadou11@gmail.com>
2022-03-18 14:48:38 +0100

    Ajout hooks useState (state isValid)

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
 
 function Form() {
@@ -7,7 +7,12 @@ function Form() {
                            password: ""
                          };
     const [formValues, setFormValues] = useState(intialValues);
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [isValid, setIsValid] = useState(false);
+    
     console.log(formValues);
+    console.log(isValid);
 
     const handleChange = (e) => { 
         console.log(e.target.value);
@@ -15,25 +20,80 @@ function Form() {
         setFormValues({...formValues, [name]:value})
     }
 
+    useEffect(() => {
+        if(Object.keys(formErrors).length === 0 && isSubmit) {
+            console.log(formValues);
+        }
+    }, [formErrors])
+
+    // Envoie du formulaire
     const handleSubmit = (e) => {
-        console.log(e.target.value);
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmit(true);
     }
  
+    // Fonction validation 
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        // Test sur le chanmp username
+        if(!values.username) { // Si le champ username est vide
+            errors.username = " Username est obligatoire";
+            setIsValid(false);
+        }else {
+            setIsValid(true);
+        }
+        // Test sur le champ email
+        if(!values.email) {
+            errors.email = " Email est obligatoire ";
+            setIsValid(false);
+        } else if(!regex.test(values.email)) {
+            errors.email = " Email invalide";
+            setIsValid(false);
+        } else {
+            setIsValid(true);
+        }
+        // Test sur le champ mot de passe
+        if(!values.password) {
+            errors.password = " Mot de passe est obligatoire";
+            setIsValid(false);
+        } else if(values.password.length < 4) {
+            errors.password = " Mot de passe doit contenir au moins 4 caractères";
+            setIsValid(false);
+        } else if(values.password.length > 10) {
+            errors.password = " Mot de passe ne doit pas depasser 10 caractères";
+            setIsValid(false);
+        } else {
+            setIsValid(true);
+        }
+
+        return errors;
+    };
+
     return (
     <div className="container-form">
+        {Object.keys(formErrors).length === 0 && isSubmit ? (
+            <div className="success">Vous êtes connecté avec succès</div>
+        ) : (
+            <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+        )}
+
         <form className='form' onSubmit={handleSubmit}>
             <div className="form-title">Connexion</div>
-            <div className="form-tited"></div>
+            <div className="form-tired"></div>
             <div className="form-group">
                 <label htmlFor="username">Username</label>
                 <input 
                   type="text" 
                   name="username" 
-                  id="username" 
+                  id="username"
+                  placeholder='Username' 
                   value={formValues.username}
                   onChange={handleChange}
                 />
             </div>
+            <p className='errors'>{formErrors.username}</p>
 
             <div className="form-group">
                 <label htmlFor="email">Email</label>
@@ -41,10 +101,12 @@ function Form() {
                   type="email" 
                   name="email" 
                   id="email" 
+                  placeholder='Email'
                   value={formValues.email}
                   onChange={handleChange}
                 />
             </div>
+            <p className='errors'>{formErrors.email}</p>
 
             <div className="form-group">
                 <label htmlFor="password">Mot de passe</label>
@@ -52,10 +114,13 @@ function Form() {
                   type="password" 
                   name="password" 
                   id="password" 
+                  placeholder='Mot de passe'
                   value={formValues.password}
                   onChange={handleChange}
                 />
             </div>
+            <p className='errors'>{formErrors.password}</p>
+
             <button className="btn-sign-up">
                 Inscription
             </button>
